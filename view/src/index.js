fea0454dcf5ec16c364b44415f09cf854d258b38
Cady <cadyzhou@outlook.com>
2022-01-15 15:47:30 -0500

    add react hotloader

--- view/src/index.js
+++ view/src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { hot } from "react-hot-loader";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const app = ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+export default hot(app);
