c4c6df2bd7d847ab9f29c30f7c6bb5fba7c79c47
Cady <cadyzhou@outlook.com>
2022-01-14 21:03:37 -0500

    ignore package-lock

--- model/src/index.js
+++ model/src/index.js
@@ -1,7 +1,11 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+const port = 3000;
 
-app.listen(3000, () =>
-  console.log("Found it")
-);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
