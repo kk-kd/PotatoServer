a23aa9fb64e0b1b276d4e81fe70a5683d7ed4554
Cady <cadyzhou@outlook.com>
2022-01-15 23:02:39 -0500

    add https connection

--- model/index.js
+++ model/index.js
@@ -1,13 +1,22 @@
-const express = require("express");
-const app = express();
 const port = 3000;
 
+var fs = require("fs");
+var https = require("https");
+var privateKey = fs.readFileSync(__dirname + "/../../cert/server.key", "utf8");
+var certificate = fs.readFileSync(
+  __dirname + "/../../cert/server.cert",
+  "utf8"
+);
+
+var credentials = { key: privateKey, cert: certificate };
+var express = require("express");
+var app = express();
+
 app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port, () => {
+  console.log(`Example app listening at https://localhost:${port}`);
 });
-
-module.exports = app;
