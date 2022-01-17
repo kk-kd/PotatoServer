634881266a95ca9474a0fa54d3e16494f8ad61a6
meganrichards3 <31023715+meganrichards3@users.noreply.github.com>
2022-01-17 16:17:25 -0500

    added dummy express api for login

--- view/src/App.js
+++ view/src/App.js
@@ -2,18 +2,26 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./headers/Header";
 import { Schools } from "./schools/Schools";
+import React, { useState } from 'react';
+import LoginPage from './login/LoginPage';
 
 export const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="Schools/*" element={<Schools />} />
-        <Route path="Users" element={<label>Users List</label>} />
-        <Route path="Students" element={<label>Students List</label>} />
-        <Route path="Routes" element={<label>Routes List</label>} />
-        <Route path="*" element={<label>Whoops</label>} />
-      </Routes>
-    </div>
-  );
+  const [token, setToken] = useState()
+  if (!token) {
+    return <LoginPage setToken={setToken} />
+  }
+  else {
+    return (
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="Schools/*" element={<Schools />} />
+          <Route path="Users" element={<label>Users List</label>} />
+          <Route path="Students" element={<label>Students List</label>} />
+          <Route path="Routes" element={<label>Routes List</label>} />
+          <Route path="*" element={<label>Whoops</label>} />
+        </Routes>
+      </div>
+    );
+  }
 }
