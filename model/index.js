e63956df6e6a7e25da4a95754fcca02dc026609a
Cady <cadyzhou@outlook.com>
2022-01-18 19:38:57 -0500

    add production lvl settings

--- model/index.js
+++ model/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.static(path.join(__dirname, "..", "view", "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "view", "build", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(3000);
 
-module.exports = app;
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+// module.exports = app;
