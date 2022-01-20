3e58bdc81d51a6402a699cf7f11482d0a86dba88
Nolan Burroughs <nolan.burroughs@duke.edu>
2022-01-19 20:33:02 -0500

    Added table format for users

--- view/src/App.js
+++ view/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./headers/Header";
 import { Schools } from "./schools/Schools";
+import { Users } from "./users/Users";
 
 export const App = () => {
   return (
@@ -9,7 +10,7 @@ export const App = () => {
       <Header />
       <Routes>
         <Route path="Schools/*" element={<Schools />} />
-        <Route path="Users" element={<label>Users List</label>} />
+        <Route path="Users/*" element={<Users />} />
         <Route path="Students" element={<label>Students List</label>} />
         <Route path="Routes" element={<label>Routes List</label>} />
         <Route path="*" element={<label>Whoops</label>} />
