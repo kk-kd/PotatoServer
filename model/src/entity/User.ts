1ee792b36fefd5a6dded0f0834b8d9166d7fca11
Cady <cadyzhou@outlook.com>
2022-01-22 22:52:57 -0500

    add password and hashing to user table

--- model/src/entity/User.ts
+++ model/src/entity/User.ts
@@ -1,6 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, Unique } from "typeorm";
+import { Length, IsNotEmpty } from "class-validator";
+import * as bcrypt from "bcryptjs";
 
-@Entity({name: "users"})
+@Entity({ name: "users" })
+@Unique(["email"])
 export class User {
   @PrimaryGeneratedColumn()
   uid: number;
@@ -38,4 +41,16 @@ export class User {
 
   @Column()
   isAdmin: boolean;
+
+  @Column()
+  @Length(8, 32)
+  password: string;
+
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+    return bcrypt.compareSync(unencryptedPassword, this.password);
+  }
 }
--- model/src/index.ts
+++ model/src/index.ts
@@ -82,51 +82,49 @@ createConnection()
       .listen(http_port);
 
     // temporary testings
-    const userRepository = connection.getCustomRepository(UserController);
-    const newUser = new User();
-    newUser.email = "test2user@email.com";
-    newUser.firstName = "test2 First Name";
-    newUser.middleName = "test2 Middle Name";
-    newUser.lastName = "test2 Last Name";
-    newUser.address = "test2 example address";
-    newUser.longitude = 101.1;
-    newUser.latitude = 102.1;
-    newUser.isAdmin = true;
-    await userRepository.save(newUser);
+    // const userRepository = connection.getCustomRepository(UserController);
+    // const newUser = new User();
+    // newUser.email = "test2user@email.com";
+    // newUser.firstName = "test2 First Name";
+    // newUser.middleName = "test2 Middle Name";
+    // newUser.lastName = "test2 Last Name";
+    // newUser.address = "test2 example address";
+    // newUser.longitude = 101.1;
+    // newUser.latitude = 102.1;
+    // newUser.isAdmin = true;
+    // await userRepository.save(newUser);
 
     // Find the person we just saved to the database using the custom query
     // method we wrote in the person repository.
-    const existingUser = await userRepository.findByUserName(
-      "test2 First Name"
-    );
-    if (!existingUser) {
-      throw Error("Unable to find test2 User entry.");
-    } else {
-      await userRepository.updateUserName(
-        existingUser.uid,
-        "Test 2 New First Name",
-        "Test 2 New Middle Name",
-        "Test 2 New Last Name"
-      );
-    }
+    // const existingUser = await userRepository.findByUserName(
+    //   "test2 First Name"
+    // );
+    // if (!existingUser) {
+    //   throw Error("Unable to find test2 User entry.");
+    // } else {
+    //   await userRepository.updateUserName(
+    //     existingUser.uid,
+    //     "Test 2 New First Name",
+    //     "Test 2 New Middle Name",
+    //     "Test 2 New Last Name"
+    //   );
+    // }
 
-    // insert new users for test
-    await connection.manager.save(
-      connection.manager.create(User, {
-        email: "jdm109@duk.edu",
-        firstName: "Jackson",
-        lastName: "McNabb",
-        address: "102 Example Lane",
-        longitude: 32.5,
-        latitude: 432.54,
-        isAdmin: false,
-      })
-    );
+    // // insert new users for test
+    // await connection.manager.save(
+    //   connection.manager.create(User, {
+    //     email: "jdm109@duk.edu",
+    //     firstName: "Jackson",
+    //     lastName: "McNabb",
+    //     address: "102 Example Lane",
+    //     longitude: 32.5,
+    //     latitude: 432.54,
+    //     isAdmin: false,
+    //   })
+    // );
 
-    console.log(
-      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
-    );
-
-    console.log();
+    // console.log(
+    //   "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+    // );
   })
   .catch((error) => console.log(error));
