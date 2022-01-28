49efd662d9f08f0e18eee7af68f7aeb369437ed0
meganrichards3 <31023715+meganrichards3@users.noreply.github.com>
2022-01-28 13:32:16 -0500

    added user create form

---
+++ view/src/api/axios_wrapper.js
@@ -0,0 +1,238 @@
+import axios from "axios";
+
+/*
+Welcome to the Axios Wrapper.
+To extract data that you need or error codes, call `.data` on the output of these calls.
+The example from ListUsers is below (as a hook):
+export const ListUsers = () => {
+  const [data] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        getAllUsers({ page: 0, size: 0, sort: "none", sortDir: "none" });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+/*
+/*
+GetAll from table (students, users, schools, routes) gives you every entry in the table.
+INPUT: {"page": page, "size": size, "sort": sort, "sortDir": sortDir}
+    page: number, denotes the number page requested; starts at 0
+    size: number, denotes the size of the page requested
+    sort: str, denotes the sort rule; options are:
+      - "none"
+      - any user entity column (firstName, middleName, lastName, etc.)
+    sortDir: str, denotes the sort direction; options are:
+      - "none"
+      - "ASC"
+      - "DESC"
+*/
+export async function getAllUsers(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/users/all/${convertMapToURL(specifications)}`
+  );
+}
+export async function getAllStudents(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/students/all/${convertMapToURL(specifications)}`
+  );
+}
+export async function getAllSchools(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/schools/all/${convertMapToURL(specifications)}`
+  );
+}
+export async function getAllRoutes(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/routes/all/${convertMapToURL(specifications)}`
+  );
+}
+
+/*
+FilterAll from table (students, users, schools, routes) gives you a filtered result from all entries.
+INPUT: {"page": page, "size": size, "sort": sort, "sortDir": sortDir}
+    page: number, denotes the number page requested; starts at 0
+    size: number, denotes the size of the page requested
+    sort: str, denotes the sort rule; options are:
+      - "none"
+      - any user entity column (firstName, middleName, lastName, etc.)
+    sortDir: str, denotes the sort direction; options are:
+      - "none"
+      - "ASC"
+      - "DESC"
+    filterType: str, denotes what you want to filter by; options are:
+      - "none"
+      - any User entity column (firstName, middleName, lastName, etc.)
+        Check out model/entities/User.ts for all options.
+*/
+export async function filterAllUsers(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/users/filter/${convertMapToURL(specifications)}`
+  );
+}
+export async function filterAllStudents(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/students/filter/${convertMapToURL(
+      specifications
+    )}`
+  );
+}
+export async function filtertAllSchools(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/schools/filter/${convertMapToURL(
+      specifications
+    )}`
+  );
+}
+export async function filterAllRoutes(specifications) {
+  return await axios.get(
+    `https://localhost:3000/api/routes/filter/${convertMapToURL(
+      specifications
+    )}`
+  );
+}
+/*
+   Returns one entry from a table (students, users, schools, routes) by UID.
+*/
+export async function getOneUser(uid) {
+  return await axios.get("https://localhost:3000/api/users/" + uid);
+}
+export async function getOneStudent(uid) {
+  return await axios.get("https://localhost:3000/api/students/" + uid);
+}
+export async function getOneSchool(uid) {
+  return await axios.get("https://localhost:3000/api/schools/" + uid);
+}
+export async function getOneRoute(uid) {
+  return await axios.get("https://localhost:3000/api/routes/" + uid);
+}
+/*
+   Deletes one entry from a table (students, users, schools, routes) by UID.
+*/
+export async function deleteUser(uid) {
+  return await axios.delete("https://localhost:3000/api/users/" + uid);
+}
+export async function deleteStudent(uid) {
+  return await axios.delete("https://localhost:3000/api/students/" + uid);
+}
+export async function deleteSchool(uid) {
+  return await axios.delete("https://localhost:3000/api/schools/" + uid);
+}
+export async function deleteRoute(uid) {
+  return await axios.delete("https://localhost:3000/api/routes/" + uid);
+}
+
+/*
+Register a User for the first time to add them to the database.
+        For a User:
+            {
+                email: "NewUniqueEmail@email.net",
+                firstName: "NewFirstName",
+                middleName: "NewMiddleName",
+                lastName: "NewLastName",
+                address: "New Address",
+                isAdmin: false,
+                password: "testnewpass",
+            }
+*/
+export async function registerUser(specifications) {
+    return {id: "7", "status": 200}
+//   return await axios.post(
+//     "https://localhost:3000/api/register",
+//     specifications
+//   );
+}
+
+/*
+Change a User Password
+        You need to specify the old and new password:
+            {
+                oldPassword: "OldPassword123",
+                newPassword: "NewPassword321",
+            }
+*/
+export async function changeUserPassword(specifications, headers) {
+  return await axios.post(
+    "https://localhost:3000/api/change-password",
+    specifications,
+    headers
+  );
+}
+
+/*
+Login Validation for a User
+        You need to specify the old and new password:
+            {
+                email: "example@email.net",
+                password: "myOldPassword",
+            }
+*/
+export async function loginUser(specifications) {
+  return await axios.post("https://localhost:3000/api/login", specifications);
+}
+
+/*
+    Saves a new entry to a table (students, users, schools, routes).
+    The INPUT is a json map of each entity property;
+        These are examples below. If you need to see the specific property names, check out 
+        model/src/entities/*.ts to see all properties
+        For a Student,
+            {
+                id: 123456,
+                firstName: "NewFirstName",
+                middleName: "NewMiddleName",
+                lastName: "NewLastName",
+            }
+        For a Route,
+            {
+                name: "NewRouteName",
+                description: "NewRouteDescription",
+            }
+        For a School:
+            {
+                name: "NewSchoolName",
+                address: "New School Address",
+                longitude: 1,
+                lattitude: 2,
+            }
+*/
+
+export async function saveStudent(specifications) {
+  return await axios.post(
+    `https://localhost:3000/api/students/${convertMapToURL(specifications)}`
+  );
+}
+export async function saveSchool(specifications) {
+  return await axios.post(
+    `https://localhost:3000/api/schools/${convertMapToURL(specifications)}`
+  );
+}
+export async function saveRoute(specifications) {
+  return await axios.post(
+    `https://localhost:3000/api/routes/${convertMapToURL(specifications)}`
+  );
+}
+/*
+   Updates an existing entry in a table (students, users, schools, routes) by UID.
+*/
+export async function updateUser(uid) {
+  return await axios.put("https://localhost:3000/api/users/" + uid);
+}
+export async function updateStudent(uid) {
+  return await axios.put("https://localhost:3000/api/students/" + uid);
+}
+export async function updateSchool(uid) {
+  return await axios.put("https://localhost:3000/api/schools/" + uid);
+}
+export async function updateRoute(uid) {
+  return await axios.put("https://localhost:3000/api/routes/" + uid);
+}
+// Helpers
+function convertMapToURL(map) {
+  return Object.keys(map)
+    .map((key) => `${key}=${map[key]}`)
+    .join("&");
+}
--- view/src/users/CreateUser.js
+++ view/src/users/CreateUser.js
@@ -2,9 +2,24 @@ import "./CreateUser.css";
 import GoogleMapReact from "google-map-react";
 import { useState } from "react";
 import { Marker } from "./../map/Marker";
+import {registerUser} from "../api/axios_wrapper";
+import { useNavigate } from "react-router-dom";
 
 export const CreateUser = () => {
+  let navigate = useNavigate();
+
+  // user
+  const [ firstName, setFirstName ] = useState("");
+  const [ middleName, setMiddleName ] = useState("");
+  const [ lastName, setLastName ] = useState("");
+  const [ userName, setUserName] = useState("");
+  const [ password, setPassword] = useState("");
+  const [ email, setEmail ] = useState("");
   const [ address, setAddress ] = useState("");
+  const [ isAdmin, setisAdmin ] = useState(false);
+  const [students, setStudents ] = useState([]);
+
+  // maps
   const [ showMap, setShowMap ] = useState(false);
   const [ lat, setLat ] = useState();
   const [ lng, setLng ] = useState();
@@ -19,6 +34,43 @@ export const CreateUser = () => {
     },
     zoom: 13
   };
+
+  async function handleCreateUser (e) {
+    e.preventDefault(); // prevents page reload on submission
+    let form_results = {
+      email: email,
+      firstName: firstName,
+      middleName: middleName,
+      lastName: lastName,
+      address: address,
+      isAdmin: isAdmin,
+      username: userName,
+      password: password,
+    }
+    console.log("Creating User with entries:")
+    console.log(form_results)
+    try {
+      let create_user_response = await registerUser(form_results).catch ((error) => {})
+      let user_id = create_user_response.id
+      let status = create_user_response.status
+      
+      if (status === 200) {
+        navigate("/Users/info/" + user_id, { replace: true });
+        throw alert ("User Successfully Created.")
+        // move to their user detail
+      }
+      if (status === 404) {
+        navigate("/Users/info:" + user_id, { replace: true });
+        throw alert ("Login Failed Because the Server was Not Reached.")
+      } 
+      else if (status === 401) {
+        throw alert ("Login Failed Because User Already Exists")
+    }
+  } catch {
+    // avoids warning
+   }
+  }
+
   const searchLocation = () => {
     geocoder.geocode( { 'address': address }, (results, status) => {
       if (status === "OK") {
@@ -26,6 +78,7 @@ export const CreateUser = () => {
         setLng(results[0].geometry.location.lng());
         setLat(results[0].geometry.location.lat());
         setError(null);
+        setAddress(address);
       } else if (status === "ZERO_RESULTS") {
         setError("No results for that address");
       } else {
@@ -50,35 +103,115 @@ export const CreateUser = () => {
   }
 
   return (
-      <div id="user_create">
+    <div>
         <h1>Create User</h1>
-        <form onSubmit={checkMap}>
-          <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-          />
-          <input
-              type="submit"
-              value="Confirm Address"
-          />
-        </form>
-        {error && (<div>{error}</div>)}
-        {showMap && (<div style={{ height: '50vh', width: '50%', display: "inline-block" }}>
-          <GoogleMapReact
-              bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_MAPS_API}` }}
-              defaultCenter={defaultProps.center}
-              defaultZoom={defaultProps.zoom}
-              yesIWantToUseGoogleMapApiInternals
-              onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-          >
-            <Marker
-                text="You're Address"
-                lat={lat}
-                lng={lng}
-            />
-          </GoogleMapReact>
-        </div>)}
-      </div>
+        <div id = "user_create_form">
+          <form onSubmit={handleCreateUser}>
+          
+          <label className="input">
+            <p>First Name:</p>
+              <input
+                  type="text"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+              />
+          </label>
+            
+          <label className="input">
+            <p>Middle Name:</p>
+              <input
+                  type="text"
+                  value={middleName}
+                  onChange={(e) => setMiddleName(e.target.value)}
+              />
+          </label>
+            
+          <label className="input">
+            <p>Last Name:</p>
+              <input
+                  type="text"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+              />
+          </label>
+          <label className="input">
+            <p>Email:</p>
+              <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+              />
+          </label>
+
+          <label className="input">
+            <p>Username:</p>
+              <input
+                  type="text"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+              />
+          </label>
+
+          <label className="input">
+            <p>Password:</p>
+              <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+              />
+          </label>
+            
+          <label className="input">
+            <p>Address:</p>
+              <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => checkMap(e.target.value)} 
+              />
+            <p> {error}</p>
+          </label>
+
+          <label className="input">
+            <p>Admin:</p>
+              <input
+                  type="checkbox"
+                  value={isAdmin}
+                  onChange={(e) => setisAdmin(e.target.value)}
+              />
+          </label>
+            
+          <label className="input">
+            <p>Students:</p>
+              <input
+                  type="text"
+                  value={students}
+                  onChange={(e) => setStudents(e.target.value)}
+              />
+          </label>
+            <div>
+              <button className = "submitbutton" type="submit">Submit</button>
+            </div>
+          </form>
+          </div>
+        <div id="user_create_map">
+          <h3> Map </h3>
+          {error && (<div>{error}</div>)}
+          {showMap && (<div style={{ height: '50vh', width: '50%', display: "inline-block" }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_MAPS_API}` }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            >
+              <Marker
+                  text="You're Address"
+                  lat={lat}
+                  lng={lng}
+              />
+            </GoogleMapReact>
+          </div>)}
+        </div>
+    </div>
   );
 }
