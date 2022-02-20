dec756b3ccb3917a66f187d205f675cd91c68b54
meganrichards3 <31023715+meganrichards3@users.noreply.github.com>
2022-02-20 14:32:40 -0500

    added reset

--- view/src/email/EmailForm.js
+++ view/src/email/EmailForm.js
@@ -4,16 +4,15 @@ import { Link, useNavigate } from "react-router-dom";
 import {
   filterAllRoutes,
   filterAllSchools,
-  sendGeneralAnnouncementToAll,
   sendGeneralAnnouncementToUsersFromSchool,
   sendGeneralAnnouncementToUsersOnRoute,
+  sendRouteAnnouncementToAll, 
+  sendGeneralAnnouncementToAll, 
 } from "../api/axios_wrapper";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { Radio } from "@mui/material";
-import { FormControlLabel } from "@mui/material";
 import { FormControl, InputLabel, Select, MenuItem, CustomSelect, StyledOption} from "@mui/material";
-import SelectUnstyled from '@mui/base/SelectUnstyled';
+
 
 
 export const EmailForm = () => {
@@ -21,6 +20,7 @@ export const EmailForm = () => {
   const [emailType, setEmailType] = useState("");
 
   const [message, setMessage] = useState({}); // keys = subject, body
+  const [includeRouteInfo, setIncludeRouteInfo] = useState(false);
 
   // school filter
   const [filteredDataSchool, setFilteredDataSchool] = useState([]);
@@ -34,15 +34,36 @@ export const EmailForm = () => {
 
   let navigate = useNavigate();
 
+  const resetState = () => {
+    setEmailType('');
+    setMessage({subject: '', body: ''});
+    setIncludeRouteInfo(false);
+    setFilteredDataSchool([]);
+    setSchoolFilter("");
+    setSelectedSchool();
+    setFilteredDataRoute([]);
+    setRouteFilter("");
+    setSelectedRoute();
+    return 1;
+  }
+
   const validate_entries = () => {
+    if (!emailType) {
+      return { valid: false, error: "Please Select A Receipient Group" };
+    }
     if (!message.subject || !message.body) {
       return { valid: false, error: "Subject and Body Required" };
+    }
+    else if (emailType == "school" && !selectedSchool) {
+      return { valid: false, error: "Please Select a School or Change Sender Type" };
+    }
+    else if (emailType == "route" && !selectedRoute) {
+      return { valid: false, error: "Please Select a Route or Change Sender Type" };
     }
     return { valid: true, error: "" };
   };
 
   const handleEmailFormSubmit = (e) => {
-    console.log("submitting!");
     let valid_results = validate_entries();
     if (valid_results.valid) {
       SendEmail(e);
@@ -63,27 +84,55 @@ export const EmailForm = () => {
         let id = selectedSchool.uid;
         console.log(id);
         console.log(form_results);
-        sendGeneralAnnouncementToUsersFromSchool({
-          message: form_results,
-          schoolId: id,
-        });
+        if (includeRouteInfo) {
+          console.log("route annoucement to users from school")
+          // sendRouteAnnouncementToUsersFromSchool({
+          //   message: form_results,
+          //   schoolId: id,
+          // });
+        }
+        else {
+          console.log("general annoucement to users from school")
+          // sendGeneralAnnouncementToUsersFromSchool({
+          //   message: form_results,
+          //   schoolId: id,
+          // });
+        }
       } else if (emailType === "route") {
         let id = selectedRoute.uid;
         console.log(id);
         console.log(form_results);
-        sendGeneralAnnouncementToUsersOnRoute({
-          message: form_results,
-          routeId: id,
-        });
+        if (includeRouteInfo) {
+          console.log("route annoucement to users on route")
+          // sendRouteAnnouncementToUsersOnRoute({
+          //   message: form_results,
+          //   routeId: id,
+          // });
+        }
+        else {
+          console.log("general annoucement to users on route")
+          // sendGeneralAnnouncementToUsersOnRoute({
+          //   message: form_results,
+          //   routeId: id,
+          // });
+        }
       } else {
         console.log(form_results);
-        sendGeneralAnnouncementToAll({ message: form_results });
+        if (includeRouteInfo) {
+          console.log("route annoucement to all users")
+          //sendRouteAnnouncementToAll({ message: form_results });
+        }
+        else {
+          console.log("general annoucement to all ")
+          //sendGeneralAnnouncementToAll({ message: form_results });
+        }
       }
     } catch (error) {
       let message = error.response.data;
       throw alert(message);
     }
     alert("Successfully Sent Email");
+    resetState();
   }
 
   useEffect(() => {
@@ -132,40 +181,40 @@ export const EmailForm = () => {
   return (
     <div id="content">
       <h1> {action_text} </h1>
-      {/* <p> </p>
       <p> </p>
-      <p> </p> */}
-
+      <p> </p>
+      <p> </p>
   
         <label id="input-label" >
             {" "}
-            Send Email To:{"     "}
+            Send Email To:{"     "} {emailType}
         </label>
-        <FormControl style={{minWidth: "10%"}} id = 'input-input' variant = "standard">
-        <Select defaultValue={"all"} onChange={(e) => {setEmailType(e.target.value);}} >
+        <FormControl  style={{minWidth: "10%"}} id = 'input-input' variant = "standard"> 
+        <Select value = {emailType} onChange={(e) => {setEmailType(e.target.value);}}  >
             <MenuItem value={"all"}> All Parents </MenuItem>
             <MenuItem value={"school"}>Parents of a Given School </MenuItem>
             <MenuItem value={"route"}>Parents of a Given Route</MenuItem>
         </Select>
                 
-        </FormControl>
+        </FormControl> 
 
       {emailType == "school" && (
         <Autocomplete
           sx={{
             paddingTop: "15px",
             paddingBottom: "10px",
-            // paddingRight: "7%",
-            maxWidth: "45%",
-            margin: "auto",
+            paddingRight: "7%",
+            maxWidth: "37%",
+            marginLeft: '52%'
           }}
           options={filteredDataSchool}
           freeSolo
           renderInput={(params) => (
-            <TextField {...params} label="Select A School" variant="standard" />
+            <TextField {...params} label="School" variant="standard" />
           )}
           getOptionLabel={(option) => option.name}
           noOptionsText={"Type to Search"}
+          
           value={selectedSchool}
           onInputChange={(e) => {
             setSchoolFilter(e.target.value);
@@ -185,11 +234,12 @@ export const EmailForm = () => {
             paddingTop: "15px",
             paddingBottom: "10px",
             paddingRight: "7%",
-            maxWidth: "50%",
-            margin: "auto",
+            maxWidth: "37%",
+            // margin: "auto",
+            marginLeft: '52%'
           }}
           renderInput={(params) => (
-            <TextField {...params} label=" Select Route " variant="standard" />
+            <TextField {...params} label=" Route " variant="standard" />
           )}
           getOptionLabel={(option) => option.name}
           fullWidth={true}
@@ -204,7 +254,18 @@ export const EmailForm = () => {
           }}
         />
       )}
-      <div></div>
+      <p> </p>
+      <p> </p>
+      <p> </p>
+      
+      <label id="input-label" > Include Student Route Information  </label>
+        <input
+            id  = "input-check"
+            style ={{paddingLeft: '10%'}}
+            type="checkbox"
+            checked ={includeRouteInfo}
+            onInput={(e) => setIncludeRouteInfo(!includeRouteInfo)}
+       />
 
       <p> </p>
       <p> </p>
@@ -222,28 +283,27 @@ export const EmailForm = () => {
       <p> </p>
       <p> </p>
 
-      <label id="input-label" for="firstName">
+      <label  >
         {" "}
         Email Body:{" "}
       </label>
       <textarea
-        id="input-input"
-        cols = "300"
-        maxLength="100"
+        style = {{width: "60%", margin: 'auto', display: 'flex'}}
+      
         value={message.body}
         onChange={(e) => setMessage({ ...message, body: e.target.value })}
       />
       <p> </p>
       <p> </p>
       <button
-        className="submitbutton"
+        id="submitbutton"
         type="button"
         onClick={(e) => {
           handleEmailFormSubmit(e);
         }}
       >
-        {" "}
-        {action_text}{" "}
+       
+        {action_text}
       </button>
     </div>
   );
