49fdff54fcdf5edccff7001c144280189d4d6057
Jackson McNabb <jackson.mcnabb@duke.edu>
2022-02-23 20:09:29 -0500

    tables updated bordered

--- view/src/users/UserDetail.js
+++ view/src/users/UserDetail.js
@@ -1,8 +1,8 @@
 import { useEffect, useMemo, useState } from "react";
-import { Link, useParams, useNavigate} from "react-router-dom";
-import {useTable } from "react-table";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useTable } from "react-table";
 import { deleteUser, getOneUser } from "../api/axios_wrapper";
-import useBatchedState from 'react-use-batched-state';
+import useBatchedState from "react-use-batched-state";
 
 export const UserDetail = () => {
   const { id } = useParams();
@@ -13,16 +13,16 @@ export const UserDetail = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetchedData = await getOneUser(id); 
+        const fetchedData = await getOneUser(id);
         setData(fetchedData.data);
         console.log(fetchedData.data);
         setStudents([fetchedData.data][0].students);
       } catch (error) {
-          let message = error.response.data;
-          throw alert (message);
-      }};
+        let message = error.response.data;
+        throw alert(message);
+      }
+    };
     fetchData();
-  
   }, []);
 
   const handleDeleteUser = (user_id, e) => {
@@ -31,135 +31,132 @@ export const UserDetail = () => {
     console.log(sName);
     console.log(data.email);
     if (!sName) {
-      return; 
+      return;
     } else if (sName.toLowerCase().trim() !== data.email.toLowerCase().trim()) {
-      alert("Entered Email Does Not Match."); 
+      alert("Entered Email Does Not Match.");
       return;
     } else {
       deleteUserCall(user_id);
-    } 
-  }
-  const deleteUserCall = async (user_id) => {  
+    }
+  };
+  const deleteUserCall = async (user_id) => {
     try {
-      await deleteUser(parseInt(user_id)); 
-      alert ("User Deletion Successful");
-      navigate('/Users/list');
-    } catch (e)  {
+      await deleteUser(parseInt(user_id));
+      alert("User Deletion Successful");
+      navigate("/Users/list");
+    } catch (e) {
       console.log(e);
       let message = e.response.data;
-      alert (message);
-    }    
-  }
- 
+      alert(message);
+    }
+  };
+
   const columns = useMemo(
-      () => [
-        {
-            Header: 'Student First Name',
-            accessor: 'firstName',
-        },
-        {
-          Header: 'Student Last Name',
-          accessor: 'lastName',
-        },
-         {
-          Header: 'Missing Route',
-          disableFilters : true,
-          accessor: 'route',
-          Cell: ({value}) => { 
-              return <ul> {value ? 'No' : 'Yes' } </ul> 
-          }
+    () => [
+      {
+        Header: "Student First Name",
+        accessor: "firstName",
       },
       {
-        Header: ' ',
+        Header: "Student Last Name",
+        accessor: "lastName",
+      },
+      {
+        Header: "Missing Route",
         disableFilters: true,
-        accessor: 'uid',
-        Cell: ({value}) => { 
-          return <Link to = {"/Students/info/" + value}> {"View Student Detail"} </Link>   
-        }
-    }
-      ],
-      []
+        accessor: "route",
+        Cell: ({ value }) => {
+          return <ul> {value ? "No" : "Yes"} </ul>;
+        },
+      },
+      {
+        Header: " ",
+        disableFilters: true,
+        accessor: "uid",
+        Cell: ({ value }) => {
+          return (
+            <Link to={"/Students/info/" + value}>
+              {" "}
+              {"View Student Detail"}{" "}
+            </Link>
+          );
+        },
+      },
+    ],
+    []
   );
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow
-  } = useTable({columns, data: students});
-
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data: students });
 
   return (
-      <div id="userListing">
-        <h1>User Detail (
-          <Link to={'/Users/edit/' + id}> Edit User </Link>,
-          <Link to={'/Users/list'} onClick = {(e) => {handleDeleteUser(id, e);} }> Delete User </Link>
-          )  </h1>
-        <h3>User Characteristics </h3>
-   
-        <div> 
-          <p>First Name : {data.firstName}</p>
-          <p>Middle Name : {data.middleName}</p>
-          <p>Last Name : {data.lastName}</p>
-          <p>Email : {data.email}</p>
-          <p>Address : {data.address}</p>
-          <p>Admin : {data.isAdmin ? "Yes" : "No"}</p>
-        </div>     
+    <div id="userListing">
+      <h1>
+        User Detail (<Link to={"/Users/edit/" + id}> Edit User </Link>,
+        <Link
+          to={"/Users/list"}
+          onClick={(e) => {
+            handleDeleteUser(id, e);
+          }}
+        >
+          {" "}
+          Delete User{" "}
+        </Link>
+        ){" "}
+      </h1>
+      <h3>User Characteristics </h3>
 
-        <h3>Students Associated With This User </h3>
-        {students.length === 0 ? <h4>There are no students attached to this user! Create one by clicking <Link to="/Users/create">here.</Link></h4>
-            :
-        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+      <div>
+        <p>First Name : {data.firstName}</p>
+        <p>Middle Name : {data.middleName}</p>
+        <p>Last Name : {data.lastName}</p>
+        <p>Email : {data.email}</p>
+        <p>Address : {data.address}</p>
+        <p>Admin : {data.isAdmin ? "Yes" : "No"}</p>
+      </div>
+
+      <h3>Students Associated With This User </h3>
+      {students.length === 0 ? (
+        <h4>
+          There are no students attached to this user! Create one by clicking{" "}
+          <Link to="/Users/create">here.</Link>
+        </h4>
+      ) : (
+        <table
+          {...getTableProps()}
+          class="table table-striped table-bordered border-success rounded"
+        >
           <thead>
-          {headerGroups.map(headerGroup => (
+            {headerGroups.map((headerGroup) => (
               <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                    <th
-                        {...column.getHeaderProps((column.id === "name" || column.id === "email_address"))}
-                        style={column.id === "name" || column.id === "email_address" ? {
-                          borderBottom: 'solid 3px red',
-                          background: 'aliceblue',
-                          color: 'black',
-                          fontWeight: 'bold',
-                          cursor: 'pointer'
-                        } : {
-                          borderBottom: 'solid 3px red',
-                          background: 'aliceblue',
-                          color: 'black',
-                          fontWeight: 'bold',
-                        }}
-                    >
-                      {column.render('Header')}
-                    </th>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps(
+                      column.id === "name" || column.id === "email_address"
+                    )}
+                  >
+                    {column.render("Header")}
+                  </th>
                 ))}
               </tr>
-          ))}
+            ))}
           </thead>
           <tbody {...getTableBodyProps()}>
-          {rows.map(row => {
-            prepareRow(row)
-            return (
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
                 <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
+                  {row.cells.map((cell) => {
                     return (
-                        <td
-                            {...cell.getCellProps()}
-                            style={{
-                              padding: '10px',
-                              border: 'solid 1px gray',
-                              background: 'papayawhip',
-                            }}
-                        >
-                            {cell.render('Cell')}
-                        </td>
-                    )
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
                   })}
                 </tr>
-            )
-          })}
+              );
+            })}
           </tbody>
-        </table> }
-      </div>
+        </table>
+      )}
+    </div>
   );
-}
+};
