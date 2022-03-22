d813168e06c83897eea3bfe49a89fc33f62c858a
Jackson McNabb <jackson.mcnabb@duke.edu>
2022-03-22 02:51:27 -0400

    student error messages done

--- view/src/import/ValidationUtils.js
+++ view/src/import/ValidationUtils.js
@@ -1,37 +1,60 @@
-
-
-export const CheckStudentCell = (col, val, schools, users, emails, schoolNames) => {
-    if (col === 'index' || col === 'valid') {
-        return ''
+export const CheckStudentCell = (
+  col,
+  val,
+  schools,
+  users,
+  emails,
+  schoolNames
+) => {
+  if (col === "index" || col === "valid") {
+    return "";
+  }
+  // catches blank cases
+  else if (val === null || val === undefined) {
+    return "Blank!";
+  }
+  // If the student isn't numerical
+  else if (col === "student_id") {
+    if (isNaN(val)) {
+      return "Not a number";
     }
-    // check for columns
-    else if (val) {
-        return '';
+  } else if (col === "parent_email") {
+    if (!emails.includes(val) && !emails.includes(val.toLowerCase().trim())) {
+      return "Email not registered";
     }
-    else {
-        return 'Empty';
+  } else if (col === "school_name") {
+    if (
+      !schoolNames.includes(val) &&
+      !schoolNames.includes(val.toLowerCase().trim())
+    ) {
+      return "School not registered";
     }
-    
-} 
+  }
+  //dodged every wrong case? Return a success.
+  return "";
+};
 
-export const CheckParentCell = (col, val, schools, users, emails, schoolNames) => {
-    
-} 
+export const CheckParentCell = (
+  col,
+  val,
+  schools,
+  users,
+  emails,
+  schoolNames
+) => {};
 
 export const CheckStudentRow = (row, schools, users, emails, schoolNames) => {
-    console.log(row)
-    
-    for (const [key, value] of Object.entries(row)) {
-        let m = CheckStudentCell(key, value)
-        if (m) {
-            console.log(key, value)
-            return m;
-        }
-    }  
-    return ''; 
-} 
+  console.log(row);
+
+  for (const [key, value] of Object.entries(row)) {
+    let m = CheckStudentCell(key, value);
+    if (m !== "") {
+      return "Error found";
+    }
+  }
+  return "";
+};
 
 export const CheckParentRow = (row, schools, users, emails, schoolNames) => {
-    return '';
-    
-} 
+  return "";
+};
